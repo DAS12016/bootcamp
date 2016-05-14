0d6e0eba8c5c69c4307a20700eb1a032336c098e
Beatriz <beatriz.216@hotmail.com>
2016-05-14 20:31:21 -0300

    Adding tests to Notification class

--- bootcamp/activities/tests.py
+++ bootcamp/activities/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from bootcamp.activities.models import Activity
+from bootcamp.activities.models import Activity, Notification
 # Create your tests here.
 
 class ActivityTestCase(TestCase):
@@ -20,3 +20,22 @@ class ActivityTestCase(TestCase):
     def testSaveNewActivity(self):
         self.activity.save();
         self.assertEqual(Activity.objects.all()[0], self.activity)
+
+
+class NotificationTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="user1", password="password")
+        User.objects.create(username="user2", password="password")
+        user1 = User.objects.get(username="user1")
+        user2 = User.objects.get(username="user2")
+
+        self.notification = Notification(notification_type='L',
+                                         from_user=user1, to_user=user2)
+        self.notification.save()
+
+    def tearDown(self):
+        for user in User.objects.all():
+            user.delete()
+
+    def testGetSummary(self):
+        self.assertEqual(Notification.objects.all()[0].get_summary("No post"), "No post")
